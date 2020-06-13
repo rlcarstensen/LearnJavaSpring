5703a29ba22da6e422a1bd0151af0491e76077da
Robert Carstensen <rlcarstensen@gmail.com>
2020-06-13 12:15:21 -0500

    completed 'Consuming a RESTful Web Service' on spring.io

---
+++ rest-service/src/main/java/com/example/restservice/Quote.java
@@ -0,0 +1,36 @@
+package com.example.restservice;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+
+    private String type;
+    private Value value;
+
+    public Quote() {}
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
